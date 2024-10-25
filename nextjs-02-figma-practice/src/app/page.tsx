d81d274ecede70553428b4fc4a4319a7b3e83193
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-25 20:48:47 +0500

    updated

--- nextjs-02-figma-practice/src/app/page.tsx
+++ nextjs-02-figma-practice/src/app/page.tsx
@@ -5,36 +5,13 @@ import Para from "@/components/Para";
 import Image from "next/image";
 import WebDevelop from "../../public/WebDevelop.svg";
 import Navbar from "@/components/Navbar";
+import Section01 from "@/components/Section01";
 
 export default function Home() {
-  const handleClick = () => {
-    alert("Button Clicked");
-  };
-
   return (
     <div>
       <Navbar />
-      <div className="flex justify-between items-center px-12">
-        <div className="w-[500px]">
-          <HeadingBr
-            heading1="Get More Done with"
-            heading2="Whitepace"
-            color="text-gray-800"
-          />
-          <Para
-            para="Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks"
-            color="text-gray-800"
-          />
-          <Button
-            text="Try Whitepace free"
-            color="bg-red-500"
-            mTop="mt-0"
-            mBottom="mb-0"
-            onClick={handleClick}
-          />
-        </div>
-        <Image src={WebDevelop} alt="Web Image" className="w-[500px]" />
-      </div>
+      <Section01 />
     </div>
   );
 }
---
+++ nextjs-02-figma-practice/src/components/Section01.tsx
@@ -0,0 +1,35 @@
+import Button from "../components/Button";
+import HeadingBr from "../components/HeadingBr";
+import Para from "../components/Para";
+import Image from "next/image";
+import WebDevelop from "../../public/WebDevelop.svg";
+
+export default function section01() {
+  const handleClick = () => {
+    alert("Button Clicked");
+  };
+
+  return (
+    <div className="flex justify-center items-center gap-12 px-12 bg-[#222222]">
+      <div className="w-[500px]">
+        <HeadingBr
+          heading1="Get More Done with"
+          heading2="Whitepace"
+          color="text-green-500"
+        />
+        <Para
+          para="Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks"
+          color="text-white"
+        />
+        <Button
+          text="Try Whitepace free"
+          color="bg-red-500"
+          mTop="mt-0"
+          mBottom="mb-0"
+          onClick={handleClick}
+        />
+      </div>
+      <Image src={WebDevelop} alt="Web Image" className="w-[500px]" />
+    </div>
+  );
+}
