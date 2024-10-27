2230c653280d5f62e975457e746d6b5f8d3e573b
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-27 20:45:00 +0500

    updated

--- class08-grid-periodic-table/src/components/TableSetup.tsx
+++ class08-grid-periodic-table/src/components/TableSetup.tsx
@@ -6,7 +6,7 @@ export default function TableSetup({
   value,
   color,
   bg,
-  secondaryColor = "text-gray-400",
+  secondaryColor = "text-gray-500",
 }: {
   classNa?: string;
   name?: string;
@@ -19,19 +19,17 @@ export default function TableSetup({
 }) {
   return (
     <div
-      className={`flex flex-col justify-center items-center ${color} bg-white text-center border-2 rounded-[3px] pt-[2px] shadow-sm ${classNa} ${bg} overflow-hidden hover:text-white`}
+      className={`flex flex-col justify-center gap-[3px] items-center ${color} bg-white text-center border-2 rounded-[3px] pt-[2px] shadow-sm ${classNa} ${bg} overflow-hidden hover:text-white`}
     >
-      <h3 className={`text-[.55rem] leading-[.7rem] h-[1.4rem] font-medium`}>
-        {name}
-      </h3>
+      <h3 className={`text-[.6rem] leading-[.7rem] font-medium`}>{name}</h3>
       <h4
         className={`text-[.6rem] leading-[.8rem] font-medium ${secondaryColor} hover:text-white`}
       >
         {no}
       </h4>
-      <h2 className={`text-[1rem] leading-[.9rem] font-bold`}>{code}</h2>
+      <h2 className={`text-[1.1rem] leading-[.9rem] font-bold`}>{code}</h2>
       <p
-        className={`text-[.55rem] leading-[.9rem] font-medium ${secondaryColor} hover:text-white`}
+        className={`text-[.6rem] leading-[.9rem] font-medium ${secondaryColor} hover:text-white`}
       >
         {value}
       </p>
